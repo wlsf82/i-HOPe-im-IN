e2293e81cc76cf4866aa974d0a1eb6d494175c1e
Walmyr <wlsf82@gmail.com>
2021-08-25 17:26:49 +0200

    Fix test that is mocking the API

--- cypress/integration/ui-tests/customer.spec.js
+++ cypress/integration/ui-tests/customer.spec.js
@@ -49,7 +49,7 @@ describe('Customers App UI', () => {
 
     context('Customer List Screen', () => {
       it('greets and shows a table with headings and five rows', () => {
-        cy.contains(`Hi ${salesRepresentative}. It is now ${currentTimestamp} and here is our customer list. Click on each of them to view their contact details.`)
+        cy.contains(`Hi ${salesRepresentative}. It is now Wed Aug 25 2021 and here is our customer list. Click on each of them to view their contact details.`)
         cy.get('table').as('table')
           .should('be.visible')
           .find('thead')
